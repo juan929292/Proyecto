d5193c6663426ba403b63f6d9796606c14683e3a
“juan929292” <juan_ignacio92@hotmail.es>
2016-11-15 09:47:52 +0100

    -a

--- includes/imprimir_pdf.php
+++ includes/imprimir_pdf.php
@@ -1,23 +1,8 @@
 <?php 
 session_start();
 
-  //Checking if we are into the OpenShift App
-  if (isset($_ENV['OPENSHIFT_APP_NAME'])) {
-    $db_user=$_ENV['OPENSHIFT_MYSQL_DB_USERNAME']; //Openshift db name OPENSHIFT_MYSQL_DB_USERNAME
-    $db_host=$_ENV['OPENSHIFT_MYSQL_DB_HOST']; //Openshift db host OPENSHIFT_MYSQL_DB_HOST
-    $db_password=$_ENV['OPENSHIFT_MYSQL_DB_PASSWORD']; //Openshift db password OPENSHIFT_MYSQL_DB_PASSWORD
-    $db_name="Cine"; //Openshift db name
-  } else {
-    $db_user="root"; //my db user
-    $db_host="localhost"; //my db host
-    $db_password=""; //my db password
-    $db_name="Cine"; //my db name
-  }
-
-?>
-<?php
-
 require('fpdf/fpdf.php');
+include_once("../db_configuration.php");
 
 $pdf = new FPDF();
 $pdf->AddPage();
@@ -62,4 +47,5 @@ while($fila = $result->fetch_object()){
 $pdf->Ln(5);
 $pdf->SetFont('Arial', 'B', 8);
 $pdf->Output();
+
 ?>
