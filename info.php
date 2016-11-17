92a6ec884b6679720fe977b477f7d594d4667975
“juan929292” <juan_ignacio92@hotmail.es>
2016-11-17 14:55:14 +0100

    final v6

--- info.php
+++ info.php
@@ -1,6 +1,7 @@
 <?php
 echo "<h1>".$_SERVER['SERVER_NAME'] ."</h1></br></br>";
 echo "<h1>".$_SERVER['SERVER_ADDR'] ."</h1></br></br>";
-var_dump($_SERVER);
+var_dump($_SERVER)."</br></br>";
+var_dump($_ENV)."</br></br>";
 
 ?>
