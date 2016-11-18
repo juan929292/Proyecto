509098c4c005713a31d6923f3076e645fa3a65d5
“juan929292” <juan_ignacio92@hotmail.es>
2016-11-18 12:52:01 +0100

    finalv8

--- db_configuration.php
+++ db_configuration.php
@@ -3,7 +3,7 @@ if (isset($_ENV['OPENSHIFT_APP_NAME'])) {
 $db_user=$_ENV['OPENSHIFT_MYSQL_DB_USERNAME'];
 $db_host=$_ENV['OPENSHIFT_MYSQL_DB_HOST'];
 $db_password=$_ENV['OPENSHIFT_MYSQL_DB_PASSWORD'];
-$db_name='instalador';
+$db_name='cine';
 } else {
 $db_user='root';
 $db_host='localhost';
