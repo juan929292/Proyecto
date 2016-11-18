8e368bbf337f37c2c75265ef7e8c9f8fca4ae3fb
“juan929292” <juan_ignacio92@hotmail.es>
2016-11-18 13:04:45 +0100

    finalv9

--- db_configuration.php
+++ db_configuration.php
@@ -1,13 +1,13 @@
 <?php
 if (isset($_ENV['OPENSHIFT_APP_NAME'])) {
-$db_user=$_ENV['OPENSHIFT_MYSQL_DB_USERNAME'];
-$db_host=$_ENV['OPENSHIFT_MYSQL_DB_HOST'];
-$db_password=$_ENV['OPENSHIFT_MYSQL_DB_PASSWORD'];
-$db_name='cine';
+$db_user=$_ENV['OPENSHIFT_MYSQL_DB_USERNAME']; //Openshift db name OPENSHIFT_MYSQL_DB_USERNAME
+$db_host=$_ENV['OPENSHIFT_MYSQL_DB_HOST']; //Openshift db host OPENSHIFT_MYSQL_DB_HOST
+$db_password=$_ENV['OPENSHIFT_MYSQL_DB_PASSWORD']; //Openshift db password OPENSHIFT_MYSQL_DB_PASSWORD
+$db_name='cine'; //Openshift db name
 } else {
-$db_user='root';
-$db_host='localhost';
-$db_password='';
-$db_name='instalador';
+$db_user='root'; //my db user
+$db_host='localhost'; //my db host
+$db_password=''; //my db password
+$db_name='cine'; //my db name
 }
 ?>
