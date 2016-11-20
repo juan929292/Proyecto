2a2f3fa0145cbe5dcdea7393b074f510202ccfbf
“juan929292” <juan_ignacio92@hotmail.es>
2016-11-20 17:37:50 +0100

    finalv9

--- db_configuration.php
+++ db_configuration.php
@@ -1,14 +1,1 @@
-<?php
-if (isset($_ENV['OPENSHIFT_APP_NAME'])) {
-$db_user=$_ENV['OPENSHIFT_MYSQL_DB_USERNAME'];
-$db_host=$_ENV['OPENSHIFT_MYSQL_DB_HOST'];
-$db_name='cine';
-$db_password=$_ENV['OPENSHIFT_MYSQL_DB_PASSWORD'];
-}
-else{
-$db_user='root';
-$db_password='';
-$db_host='localhost';
-$db_name='cine';
-}
-?>
+
