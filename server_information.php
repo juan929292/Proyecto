20004b29caf5c2e587865b51be455c3b0e3f5841
“juan929292” <juan_ignacio92@hotmail.es>
2016-11-22 02:25:25 +0100

    finalv10

--- server_information.php
+++ server_information.php
@@ -2,13 +2,11 @@
 echo "<h6 style='padding:1% 1% 1% 1%;color:black;background-color:white;border-radius:10px;'>";
 $hname=$_SERVER['SERVER_NAME'];
 $soft=$_SERVER['SERVER_SOFTWARE'];
-$ipv=$_SERVER['REMOTE_ADDR'];
 echo "INFORMACIÓN SERVIDOR:"."</br></br>";
 	if(isset($_ENV['OPENSHIFT_APP_NAME'])){
 		$ip=$_ENV['OPENSHIFT_MYSQL_DB_HOST'];
 		$pas=$_ENV['OPENSHIFT_MYSQL_DB_PASSWORD'];
 		$us=$_ENV['OPENSHIFT_MYSQL_DB_USERNAME'];
-		echo "TU IP  ------------------------> ".$ipv."</br></br>";
 		echo "IP SERVIDOR -----------------------> ".$ip."</br></br>";
 		echo "PASS SERVIDOR ------------------> ".$pas."</br></br>";
 		echo "NOMBRE HOST SERVIDOR ----> ".$hname."</br></br>";
@@ -16,7 +14,6 @@ echo "INFORMACIÓN SERVIDOR:"."</br></br>";
 	}
 	else{
 		$ip=$_SERVER['SERVER_ADDR'];
-		echo "TU IP  -----------------------> ".$ipv."</br></br>";
 		echo "IP SERVIDOR -----------------------> ".$ip."</br></br>";
 		echo "NOMBRE HOST SERVIDOR ----> ".$hname."</br></br>";
 		echo "SOFTWARE SERVIDOR ----------> ".$soft."</br></br>";
